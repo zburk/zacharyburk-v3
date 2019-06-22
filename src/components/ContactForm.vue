61ccb2f23669bd35a8bb8b921a4ee8b432e49807
Zachary Burk <zachburk7@gmail.com>
2019-06-22 15:30:56 -0400

    Wire up form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,37 +1,37 @@
 <template>
   <div>
-    <div class="p-8">
-      <h1 class="text-gray-900 font-bold text-xl mb-2">Contact</h1>
-      <form autocomplete="off" @submit.prevent="contact">
-        <div class="form-control">
-          <input
-            v-model="formData.name"
-            placeholder="What's your name?" />
-        </div>
+    <form autocomplete="off" method="POST" netlify>
+      <div class="p-8">
+        <h1 class="text-gray-900 font-bold text-xl mb-2">Contact</h1>
+          <div class="form-control">
+            <input
+              v-model="formData.name"
+              placeholder="What's your name?" />
+          </div>
 
-        <div class="form-control">
-          <input
-            v-model="formData.contact"
-            placeholder="How should I contact you?" />
-            <span class=" pl-2 text-xs text-gray-600">Enter your email, phone number, etc</span>
-        </div>
+          <div class="form-control">
+            <input
+              v-model="formData.contact"
+              placeholder="How should I contact you?" />
+              <span class=" pl-2 text-xs text-gray-600">Enter your email, phone number, etc</span>
+          </div>
 
-        <div class="form-control">
-          <textarea
-            v-model="formData.message"
-            placeholder="Send me some info!">
-          </textarea>
-        </div>
-      </form>
-    </div>
-    <div class="bg-gray-200 p-4 text-right">
-      <a @click="$emit('cancel')" class="text-red-400 font-bold py-2 px-4 cursor-pointer">Cancel</a>
-      <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
-        type="button">
-        Send
-      </button>
-    </div>
+          <div class="form-control">
+            <textarea
+              v-model="formData.message"
+              placeholder="Send me some info!">
+            </textarea>
+          </div>
+      </div>
+      <div class="bg-gray-200 p-4 text-right">
+        <a @click="$emit('cancel')" class="text-red-400 font-bold py-2 px-4 cursor-pointer">Cancel</a>
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
+          type="submit">
+          Send
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -50,7 +50,7 @@ export default {
 
 <style scoped>
   .form-control {
-    @apply p-2
+    @apply pt-2 pr-2 pb-2
   }
 
   input, textarea {
